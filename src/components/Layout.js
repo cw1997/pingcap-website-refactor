dd93ffc762c9f6c995f69973391866aff64f82cd
昌维 <867597730@qq.com>
2020-10-30 14:29:30 +0800

    add Layout style

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,16 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
 
+import './Layout.sass'
+
 
 export default function Layout(props) {
   return (
     <div className="Layout">
       <NavBar/>
-      {props.children}
+      <main className="main">
+        {props.children}
+      </main>
       <Footer/>
     </div>
   )
